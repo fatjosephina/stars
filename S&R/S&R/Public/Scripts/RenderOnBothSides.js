80557cdbf29f08e9dceb71b3213562d8b4c0b8b3
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2023-04-16 20:47:03 -0500

    Got rid of duplicate project

--- S&R/S&R/Public/Scripts/RenderOnBothSides.js
+++
@@ -1,5 +0,0 @@
-// -----JS CODE-----
-// @input bool twoSided = true
-
-var imageComponent = script.getSceneObject().getComponent("Component.Image");
-imageComponent.mainPass.twoSided = script.twoSided;
